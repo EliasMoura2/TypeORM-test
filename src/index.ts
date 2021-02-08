2101f5d4edbfe7505d35634d2f2303f45bf0d838
Elias Moura <d.eliasmoura2@gmail.com>
2021-02-08 19:27:31 -0300

    oneToOne relation

--- src/index.ts
+++ src/index.ts
@@ -25,23 +25,37 @@ createConnection({
     logging: false
 }).then(async connection => {
     // here you can start to work with your entities
-    
-    console.log("Inserting a new user into the database..."); 
-    
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    user.isActive = true;
-    const userRepository = connection.getRepository(User);
-    await userRepository.save(user);
-    // await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
-    
 
-    console.log("Loading users from the database...");
-    // const users = await connection.manager.find(User);
-    // const allUsers = await repository.find();
-    const users = await userRepository.find();
-    console.log("Loaded users: ", users);
+    // console.log("Here you can setup and run express/koa/any other framework.");
+
+    // create a photo
+    let photo = new Photo();
+    photo.name = "Me and Bears";
+    photo.description = "I am near polar bears";
+    photo.filename = "photo-with-bears.jpg";
+    photo.views = 1;
+    photo.isPublished = true;
+
+    // create a photo metadata
+    let metadata = new PhotoMetadata();
+    metadata.height = 640;
+    metadata.width = 480;
+    metadata.orientation = "portrait";
+    metadata.compressed = true;
+    metadata.comment = "cybershoot";
+    metadata.photo = photo; // this way we connect them
+
+    // get entity repositories
+    let photoRepository = connection.getRepository(Photo);
+    let metadataRepository = connection.getRepository(PhotoMetadata);
+
+    // first we should save a photo
+    await photoRepository.save(photo);
+
+    // photo is saved. Now we need to save a photo metadata
+    await metadataRepository.save(metadata);
+
+    // done
+    console.log("Metadata is saved, and relation between metadata and photo is created in the database too");
+
 }).catch(error => console.log(error));
